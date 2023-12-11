b39be8b04b6e3e22ad6bcf7ea2c03e187471468c
Nastya <nastya.eremina.98@gmail.com>
2023-12-12 01:38:57 +0300

    SimpleNumber

---
+++ TasksPractice/TasksPractice/PrimeNumberCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PrimeNumberCheck
+    {
+        public void CheckNumber()
+        {
+            string input;
+
+            Console.Write("Простое ли число: ");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine(IsSimple(number) ? "Простое число" : "Не простое число");
+            }
+            else
+            {
+                Console.WriteLine("Введено не целое число!");
+            }
+
+            bool IsSimple(int number)
+            {
+                if (number <= 1) return false;
+
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
