47d538e39a568cda5b403b736d4d22bf5d0ae9e3
Nastya <nastya.eremina.98@gmail.com>
2023-12-12 02:10:46 +0300

    AddReverxeString

--- TasksPractice/TasksPractice/Program.cs
+++ TasksPractice/TasksPractice/Program.cs
@@ -1,3 +1,16 @@
 ﻿using System;
 
+Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+
+char[] inputToChars = input.ToCharArray();
+
+Console.Write("Answer One: "+ ReverseString(inputToChars));
+
+string ReverseString(char[] chars)
+{
+    Array.Reverse(chars);
+
+    return new string (chars);
+}
 
